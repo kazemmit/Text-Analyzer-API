dc5bae1ce814d26ac3ee4ff61b3b6f4150c6811d
Kazem Qazanfari <kazemqazanfari@Kazems-MacBook-Pro.local>
2019-06-26 22:32:51 -0400

    update

--- TS_Sentiment.py
+++ TS_Sentiment.py
@@ -1,8 +1,11 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 def Text_Sentiment(text):
+    '''
+    comments needed
+    :param text:
+    :return:
+    '''
     sent_analyzer = SentimentIntensityAnalyzer()
     sent_analyzed = sent_analyzer.polarity_scores(text)
-
     result = {'negativity':sent_analyzed['neg'],'positivity':sent_analyzed['pos']}
     return result
